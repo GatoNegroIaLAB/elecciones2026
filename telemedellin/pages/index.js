10510b2993f2d312857d4fe9b0eb730178841603
LokiGatoNegro <loki@gatonegroialab.local>
2026-03-06 15:24:42 +0000

    Enable LIVE_MODE for registraduria simulation

--- telemedellin/pages/index.js
+++ telemedellin/pages/index.js
@@ -189,7 +189,7 @@ export default function Home() {
     }
   }, [corporacion])
 
-  const LIVE_MODE = false // Cambiar a true el día del simulacro y elecciones
+  const LIVE_MODE = true // Activado para simulacro y elecciones
 
   useEffect(() => {
     if (!LIVE_MODE) {
